679d92e4bf661f7cb8be278c91dc816e3f0e8aef
ivkic <ivonazivanov96@gmail.com>
2019-09-10 18:41:57 +0200

    Add files via upload

---
+++ Klase podataka/KlasePodataka/KlasePodataka/clsLokacija.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KlasePodataka
+{
+    public class clsLokacija
+    {
+        // atributi
+        private string pSifra;
+        private string pNaziv;
+
+        // property
+        public string Sifra
+        {
+            get
+            {
+                return pSifra;
+            }
+            set
+            {
+                if (this.pSifra != value)
+                    this.pSifra = value;
+            }
+        }
+
+        public string Naziv
+        {
+            get
+            {
+                return pNaziv;
+            }
+            set
+            {
+                if (this.pNaziv != value)
+                    this.pNaziv = value;
+            }
+        }
+
+
+        // konstruktor
+        public clsLokacija()
+        {
+            pSifra = "";
+            pNaziv = "";
+        }
+
+        
+    }
+}
